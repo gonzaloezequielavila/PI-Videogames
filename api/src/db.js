b0195aedabdd2040e76848fd371653f1ce2de9e0
Gonza <gonzaloezequielavila@gmail.com>
2023-12-13 20:58:38 -0300

    added models and modified db

--- api/src/db.js
+++ api/src/db.js
@@ -30,12 +30,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogame, Genre } = sequelize.models;
 
 // Aca vendrian las relaciones
+Videogame.belongsToMany(Genre, { through: 'videogame_genre' });
+Genre.belongsToMany(Videogame, { through: 'videogame_genre' });
 // Product.hasMany(Reviews);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  conn: sequelize,     // para importar la conexión { conn } = require('./db.js');
 };
--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    plarforms: {
+      type:DataTypes.ENUM('PC', 'PlayStation 4', 'Xbox One', 'Nintendo Switch', 'iOS', 'Android'),
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    launch_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+
+    },
+  },
+  { timestamps: false });
 };
