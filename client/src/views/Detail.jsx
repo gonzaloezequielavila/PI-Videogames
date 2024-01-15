f35b26d9b5674757aaed1ef5aa15787383bbb9b2
Gonza <gonzaloezequielavila@gmail.com>
2024-01-15 16:53:22 -0300

    added general views

--- client/src/views/Detail.jsx
+++ client/src/views/Detail.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import styles from './Detail.module.css'
 
 const Detail = () => {
   return(
-    <>
-    <h1>Detail Page</h1>
-    </>
+    <div className={styles.container}>
+    <h1>Game Name (Detail)</h1>
+    <div className={styles.card}>
+      <h3>IMAGE</h3>
+      <h3>description</h3>
+      <h3>Platforms</h3>
+      <h3>Release Date</h3>
+      <h3>Rating</h3>
+    </div>
+
+    </div>
   )
 };
 
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './Home.module.css'
 
 const Home = () => {
   return(
-    <div>
+    <div className={styles.container}>
       <h1>This is the home page</h1>
+      <div className={styles.searchBar}>
+        <input></input>
+        <button>Search</button>
+      </div>
     </div>
   )
 }
