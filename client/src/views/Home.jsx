42eb36db2cf270d91c7225d791f7fd29973035fc
Gonza <gonzaloezequielavila@gmail.com>
2024-01-15 21:16:21 -0300

    added styles to views

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import styles from './Home.module.css'
 
 const Home = () => {
+
+  const message = () => {
+    alert('This button will search')
+  }
+
   return(
     <div className={styles.container}>
       <h1>This is the home page</h1>
       <div className={styles.searchBar}>
-        <input></input>
-        <button>Search</button>
+        <input className={styles.input} placeholder='Search a Game'></input>
+        <button onClick={message}>Search</button>
       </div>
     </div>
   )
--- client/src/views/NewGame.jsx
+++ client/src/views/NewGame.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import styles from './NewGame.module.css'
 
 const NewGame = () => {
+
+  const message = () =>{
+    alert('This button will create a new game')
+  }
   return (
-    <div>
-      <form>
-        <input name='name'/>
-        <button type='submit'>Create</button>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input className={styles.input} name='name' placeholder='Name'/>
+        <input className={styles.input} name='description' placeholder='Description'/>
+        <input className={styles.input} name='platforms' placeholder='Platforms'/>
+        <input className={styles.input} name='image' placeholder='Image'/>
+        <input className={styles.input} name='released' placeholder='Released Date'/>
+        <input className={styles.input} name='rating' placeholder='Rating'/>
+        <button className={styles.button} onClick={message}>Create</button>
       </form>
     </div>
   )
